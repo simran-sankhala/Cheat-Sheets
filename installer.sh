74fe1a21da4f7c2dfca22a15a2b5138fb9301040
Simran Sankhala <63577507+simran-sankhala@users.noreply.github.com>
2023-01-23 21:50:19 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ sudo apt install fd-find fzf
 curl https://sh.rustup.rs -sSf | sh
 cargo install --locked navi
 cargo install feroxbuster rustscan
-navi repo add esp0xdeadbeef/cheat.sheets
+navi repo add simran-sankhala/Cheat-Sheets
 echo 'eval "$(navi widget zsh)"' >> .zshrc
 echo 'eval "$(navi widget bash)"' >> .bashrc
-echo " Installation Done ! Enjoy Our Tool "
+echo " Installation Done ! Enjoy The Tool "
